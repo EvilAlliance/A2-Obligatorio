59b1edfa382c52e725d5d3a396ad6cadcb3ce951
EvilAlliance <chialanza17@gmail.com>
2024-11-26 10:44:41 -0300

    Ejercicio4 con hashheap

--- ejercicio4.cpp
+++ ejercicio4.cpp
@@ -1,41 +1,46 @@
 #include <cassert>
-#include <string>
 #include <iostream>
 #include <limits>
+#include <string>
 #include <utility>
 
-#include "./src/heap.cpp"
-#include "./src/HashTable.cpp"
+#include "./src/HashHeap.cpp"
 
 using namespace std;
 
 struct key {
-    int id;
-    int prioridad;
-    bool paraLlevar;
+  int id;
+  int prioridad;
+  bool paraLlevar;
 };
 
 struct Order {
-    int prioridad;
-    bool paraLlevar;
-    string items;
+  int prioridad;
+  bool paraLlevar;
+  string items;
 };
 
-int compareKey(key k1, key k2){
-    if(k1.prioridad < k2.prioridad ) return -1;
-    if(k1.prioridad > k2.prioridad ) return 1;
+int compareKey(key k1, key k2) {
+  if (k1.prioridad < k2.prioridad)
+    return -1;
+  if (k1.prioridad > k2.prioridad)
+    return 1;
 
-    if(k1.paraLlevar && !k2.paraLlevar) return -1;
-    if(!k1.paraLlevar && k2.paraLlevar) return 1;
+  if (k1.paraLlevar && !k2.paraLlevar)
+    return -1;
+  if (!k1.paraLlevar && k2.paraLlevar)
+    return 1;
 
-    if(k1.id < k2.id) return -1;
-    if(k1.id > k2.id) return 1;
+  if (k1.id < k2.id)
+    return -1;
+  if (k1.id > k2.id)
+    return 1;
 
-    return 0;
+  return 0;
 }
 
 // Extraido https://gist.github.com/badboy/6267743
-unsigned long long hash32shift(int key){
+unsigned long long hash32shift(int key) {
   unsigned long long k = key;
   k = ~k + (k << 15); // k = (k << 15) - k - 1;
   k = k ^ (k >> 12);
@@ -47,104 +52,114 @@ unsigned long long hash32shift(int key){
 }
 
 // Extraido https://gist.github.com/badboy/6267743
-unsigned long long hash32shiftmult(int key){
-    unsigned long long k = key;
-    int c2=0x27d4eb2d; // a prime or an odd constant
-    k = (k ^ 61) ^ (k >> 16);
-    k = k + (k << 3);
-    k = k ^ (k >> 4);
-    k = k * c2;
-    k = k ^ (k >> 15);
-    return k;
+unsigned long long hash32shiftmult(int key) {
+  unsigned long long k = key;
+  int c2 = 0x27d4eb2d; // a prime or an odd constant
+  k = (k ^ 61) ^ (k >> 16);
+  k = k + (k << 3);
+  k = k ^ (k >> 4);
+  k = k * c2;
+  k = k ^ (k >> 15);
+  return k;
 }
 
-bool equal(int a, int b){
-    return a == b;
+bool equal(int a, int b) { return a == b; }
+
+// Extraido https://gist.github.com/badboy/6267743
+unsigned long long hash32shift1(key key) {
+  unsigned long long k = key.id;
+  k = ~k + (k << 15); // k = (k << 15) - k - 1;
+  k = k ^ (k >> 12);
+  k = k + (k << 2);
+  k = k ^ (k >> 4);
+  k = k * 2057; // k = (k + (k << 3)) + (k << 11);
+  k = k ^ (k >> 16);
+  return k;
 }
 
-int main()
-{
-    int pedidos;
-    std::cin >> pedidos;
+// Extraido https://gist.github.com/badboy/6267743
+unsigned long long hash32shiftmult1(key key) {
+  unsigned long long k = key.id;
+  int c2 = 0x27d4eb2d; // a prime or an odd constant
+  k = (k ^ 61) ^ (k >> 16);
+  k = k + (k << 3);
+  k = k ^ (k >> 4);
+  k = k * c2;
+  k = k ^ (k >> 15);
+  return k;
+}
 
-    int operaciones;
-    std::cin >> operaciones;
+bool equal1(key a, key b) { return a.id == b.id; }
 
-    Heap<key, int, void>* h = new Heap<key, int, void>(pedidos, compareKey, 0);
-    HashTable<int, Order, void>* ids = new HashTable<int, Order,void>(pedidos, equal, hash32shift, hash32shiftmult);
+int main() {
+  int pedidos;
+  std::cin >> pedidos;
 
-    for(int i = 0; i < operaciones; i++){
-        string op;
-        std::cin >> op;
-        if(op.compare("I") == 0){
-            int id;
-            int prioridad;
-            string paraLlevarS;
-            string p;
+  int operaciones;
+  std::cin >> operaciones;
 
-            std::cin >> id;
-            std::cin >> prioridad;
-            std::cin >> paraLlevarS;
+  HashHeap<key, int, void> *h = new HashHeap<key, int, void>(
+      pedidos, compareKey, 0, equal1, hash32shift1, hash32shiftmult1);
+  HashTable<int, Order, void> *ids = new HashTable<int, Order, void>(
+      pedidos, equal, hash32shift, hash32shiftmult);
 
-            bool paraLlevar = 0;
+  for (int i = 0; i < operaciones; i++) {
+    string op;
+    std::cin >> op;
+    if (op.compare("I") == 0) {
+      int id;
+      int prioridad;
+      string paraLlevarS;
+      string p;
 
-            if(paraLlevarS.compare("true") == 0) paraLlevar = 1;
+      std::cin >> id;
+      std::cin >> prioridad;
+      std::cin >> paraLlevarS;
 
-            key k = {
-                .id = id,
-                .prioridad = prioridad,
-                .paraLlevar = paraLlevar
-            };
+      bool paraLlevar = 0;
 
-            h->push(k, id);
+      if (paraLlevarS.compare("true") == 0)
+        paraLlevar = 1;
 
-            std::cin >> p;
+      key k = {.id = id, .prioridad = prioridad, .paraLlevar = paraLlevar};
 
-            Order o = {
-                .prioridad = prioridad,
-                .paraLlevar = paraLlevar,
-                .items = p
-            };
+      h->push(k, id);
 
-            ids->set(id, o);
-        }else if(op.compare("E") == 0){
-            int id;
-            std::cin >> id;
+      std::cin >> p;
 
-            ids->remove(id);
-        }else if(op.compare("C") == 0){
-            int id;
-            std::cin >> id;
+      Order o = {.prioridad = prioridad, .paraLlevar = paraLlevar, .items = p};
 
-            for(unsigned long long i = 0; i < h->count; i++){
-                key* k = &h->items[i].piority;
-                if(k->id == id){
-                    Order* o = ids->get(id);
+      ids->set(id, o);
+    } else if (op.compare("E") == 0) {
+      int id;
+      std::cin >> id;
 
-                    k->paraLlevar = !k->paraLlevar;
-                    o->paraLlevar = k->paraLlevar;
+      ids->remove(id);
+    } else if (op.compare("C") == 0) {
+      key k;
+      std::cin >> k.id;
 
-                   if(k->paraLlevar){
-                       h->levitate(i);
-                   }else{
-                       h->sink(i);
-                   }
+      HashHeap_Cell<key, int>* c = h->getCell(k);
 
-                   break;
-                }
-            }
+      Order *o = ids->get(k.id);
 
-        }
+      c->piority.paraLlevar = !c->piority.paraLlevar;
+      o->paraLlevar = c->piority.paraLlevar;
+
+      h->balance(k);
     }
+  }
 
-    while (h->count) {
-        int id = h->pop();
-        Order* o = ids->get(id);
+  while (h->count) {
+    int id = h->pop();
+    Order *o = ids->get(id);
 
-        if(o != NULL){
-            std::cout << id << " " << o->prioridad << " " << (o->paraLlevar ? "true" : "false") << " " << o->items << '\n';
-        }
+    if (o != NULL) {
+      std::cout << id << " " << o->prioridad << " "
+                << (o->paraLlevar ? "true" : "false") << " " << o->items
+                << '\n';
     }
+  }
 
-    return 0;
+  return 0;
 }
---
+++ src/HashHeap.cpp
@@ -0,0 +1,220 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <iostream>
+
+#include "./HashTable.cpp"
+
+template <class K, class V> struct HashHeap_Cell {
+  V value;
+  K piority;
+  HashHeap_Cell(K k, V v) : piority(k), value(v) {}
+  HashHeap_Cell() : piority((K){0}), value((V){0}) {}
+};
+
+template <class K, class V, class X> class HashHeap {
+public:
+  HashHeap_Cell<K, V> *items;
+  unsigned long long count = 0;
+  unsigned long long capacity;
+
+  /**
+   * -1 cunado el primero es menor
+   *  0 cunado son iguales
+   *  1 cuando el primero es mayor
+   * */
+  int (*comparePiority)(K, K);
+  int expectedRoot;
+
+  X *data;
+  void (*insertHandler)(K, V *, X *);
+  void (*removeHandler)(K, V, X *);
+
+  HashTable<K, unsigned long long, void>* h;
+
+  HashHeap(unsigned long long len, int (*comparepiority)(K, K), bool maxHeap,
+       bool(*e)(K, K), unsigned long long (*f)(K), unsigned long long (*s)(K),
+       X *d, void (*inserthandler)(K, V *, X *) = NULL,
+       void (*removehandler)(K, V, X *) = NULL) {
+    items = new HashHeap_Cell<K, V>[len]();
+    comparePiority = comparepiority;
+    expectedRoot = maxHeap ? 1 : -1;
+    capacity = len;
+
+    data = d;
+    insertHandler = inserthandler;
+    removeHandler = removehandler;
+
+    h = new HashTable<K, unsigned long long, void>(len,e, f, s);
+  }
+
+  HashHeap(unsigned long long len, int (*comparepiority)(K, K), bool maxHeap,
+       bool(*e)(K, K), unsigned long long (*f)(K), unsigned long long (*s)(K),
+       void (*inserthandler)(K, V *, X *) = NULL,
+       void (*removehandler)(K, V, X *) = NULL) {
+    items = new HashHeap_Cell<K, V>[len]();
+    comparePiority = comparepiority;
+    expectedRoot = maxHeap ? 1 : -1;
+    capacity = len;
+
+    insertHandler = inserthandler;
+    removeHandler = removehandler;
+
+    h = new HashTable<K, unsigned long long, void>(len, e, f, s);
+  }
+
+  ~HashHeap() {}
+
+  unsigned long long childsOfLetf(unsigned long long i) { return 2 * i + 1; }
+
+  unsigned long long childsOfRight(unsigned long long i) { return 2 * i + 2; }
+
+  unsigned long long parentOf(unsigned long long i) {
+    return floor((float)(i - 1) / 2.0);
+  }
+
+  void sink(unsigned long long i) {
+    assert(i >= 0);
+    if (i == count - 1)
+      return;
+
+    unsigned long long cL = childsOfLetf(i);
+    unsigned long long cR = cL + 1;
+    while (cL < count) {
+      if (cR < count && comparePiority(items[cL].piority, items[cR].piority) !=
+                            expectedRoot) {
+        cL = cR;
+      }
+      if (comparePiority(items[cL].piority, items[i].piority) == expectedRoot) {
+        HashHeap_Cell<K, V> temp = items[i];
+        items[i] = items[cL];
+        items[cL] = temp;
+
+        h->set(items[i].piority, i);
+        h->set(temp.piority, cL);
+      }
+      i = cL;
+      cL = childsOfLetf(cL);
+      cR = cL + 1;
+    }
+  }
+
+  void levitate(unsigned long long i) {
+    assert(i >= 0);
+
+    unsigned long long p;
+
+    while (i != 0 && comparePiority(items[(p = parentOf(i))].piority,
+                                    items[i].piority) != expectedRoot) {
+      HashHeap_Cell<K, V> temp = items[i];
+      items[i] = items[p];
+      items[p] = temp;
+
+      h->set(temp.piority, p);
+      h->set(items[i].piority, i);
+
+      i = p;
+    }
+  }
+
+  void push(K piority, V val) {
+    assert(count <= capacity);
+    HashHeap_Cell<K, V> *c = &items[count];
+
+    h->set(piority, count);
+
+    c->piority = piority;
+    c->value = val;
+
+    levitate(count);
+
+    count++;
+
+    if (insertHandler != NULL)
+      insertHandler(piority, &val, data);
+  }
+
+  V pop() {
+    HashHeap_Cell<K, V> c = items[0];
+
+    h->remove(c.piority);
+
+    count--;
+    items[0] = items[count];
+
+    h->set(items[0].piority, 0);
+
+    sink(0);
+
+    if (removeHandler != NULL)
+      removeHandler(c.piority, c.value, data);
+
+    return c.value;
+  }
+
+  V *peek() { 
+    if(count == 0) return NULL;
+    return &items[0].value;
+  }
+
+  V replaceTop(K piority, V val) {
+    HashHeap_Cell<K, V> c = items[0];
+
+    h->remove(c.piority);
+
+    
+    HashHeap_Cell<K, V> *cell = &items[0];
+    cell->piority = piority;
+    cell->value = val;
+
+    h->set(piority, 0);
+
+    sink(0);
+
+    return c.value;
+  }
+
+  V *get(K k) {
+    unsigned long long* x = h->get(k);
+    if(x == NULL) return NULL;
+    return &items[*x].value;
+  }
+
+  HashHeap_Cell<K, V>* getCell(K k) {
+    unsigned long long* x = h->get(k);
+    if(x == NULL) return NULL;
+    return &items[*x];
+  }
+
+  void balance(K k){
+    unsigned long long* i = h->get(k);
+    if(i == NULL) return;
+    sink(*i);
+    levitate(*i);
+  }
+
+  V remove(K k) {
+    unsigned long long *ip = h->get(k);
+    if(ip == NULL) return NULL;
+
+    h->remove(k);
+
+    unsigned long long i = *ip;
+
+    assert(comparePiority(items[i], k));
+
+    HashHeap_Cell<K, V> c = items[i];
+    items[i] = items[--count];
+
+    h->set(items[i].piority, i);
+
+    sink(i);
+
+    if (removeHandler != NULL)
+      removeHandler(k, c.value, data);
+  }
+
+    void reset(){
+        count = 0;
+    }
+};
